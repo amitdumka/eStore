c0c1e6825d005c8bb0935ab8ef6206bc41003cc7
Amit Kumar <amitnarayansah@gmail.com>
2021-01-29 10:36:28 +0530

    Roles added and  StoreOperations View added

---
+++ eStore/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,234 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using eStore.DL.Data;
+using eStore.Shared.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using eStore.Shared.Models.Identity;
+using Microsoft.AspNetCore.Http;
+
+namespace eStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin,PowerUser")]
+    public class RoleController : Controller
+    {
+        private readonly eStoreDbContext _context;
+
+        public RoleController(eStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        readonly RoleManager<IdentityRole> roleManager;
+        readonly UserManager<AppUser> UserManager;
+        public RoleController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> um)
+        {
+            this.roleManager = roleManager;
+            UserManager = um;
+        }
+        // GET: Role
+        public IActionResult Index()
+        {
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        // GET: Role/Details/5
+        public IActionResult Details(string id)
+        {
+
+            var roles = roleManager.FindByIdAsync(id).Result;
+            var onRols = UserManager.GetUsersInRoleAsync(roles.Name).Result;
+            ViewBag.RoleName = roles.Name;
+
+
+
+            return View(onRols);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult AssignRole()
+        {
+            //var roles = roleManager.Roles.ToList ();
+            //var userList = UserManager.Users.ToList ();
+
+            ViewData["RoleId"] = new SelectList(roleManager.Roles, "Id", "Name");
+            ViewData["UserId"] = new SelectList(UserManager.Users, "Id", "UserName");
+
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> AssignRole([Bind("UserId,RoleId")] RoleUserView ruView)
+        {
+            if (ModelState.IsValid)
+            {
+                var _user = await UserManager.FindByIdAsync(ruView.UserId);
+                var _role = await roleManager.FindByIdAsync(ruView.RoleId);
+                if (_user != null && _role != null)
+                {
+                    var a = await UserManager.AddToRoleAsync(_user, _role.Name);
+                    if (a.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ViewData["RoleId"] = new SelectList(roleManager.Roles, "Id", "Name", ruView.RoleId);
+                        ViewData["UserId"] = new SelectList(UserManager.Users, "Id", "UserName", ruView.UserId);
+                        ViewBag.ErrorMessage = "Failed to assign, try again";
+                        return View(ruView);
+                    }
+
+                }
+                else
+                {
+                    ViewData["RoleId"] = new SelectList(roleManager.Roles, "Id", "Name", ruView.RoleId);
+                    ViewData["UserId"] = new SelectList(UserManager.Users, "Id", "UserName", ruView.UserId);
+                    ViewBag.ErrorMessage = "role or  user not found, try again";
+                    return View(ruView);
+                }
+            }
+            ViewData["RoleId"] = new SelectList(roleManager.Roles, "Id", "Name", ruView.RoleId);
+            ViewData["UserId"] = new SelectList(UserManager.Users, "Id", "UserName", ruView.UserId);
+            return View(ruView);
+
+        }
+        // GET: Role/Create
+        public ActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+
+        // POST: Role/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Role/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Role/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Role/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteAsync(string id)
+        {
+            try
+            {
+
+                var role = await roleManager.FindByIdAsync(id).ConfigureAwait(true);
+
+                var res = await roleManager.DeleteAsync(role);
+                if (res.Succeeded)
+                    return RedirectToAction(nameof(Index));
+                else
+                {
+                    //TODO: check with other delete options
+                    ViewBag.ErrorMessage = "Failed to delete, try again";
+                    ViewBag.ID = id;
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult RemoveRole()
+        {
+            //var roles = roleManager.Roles.ToList ();
+            //var userList = UserManager.Users.ToList ();
+
+            ViewData["RoleId"] = new SelectList(roleManager.Roles, "Id", "Name");
+            ViewData["UserId"] = new SelectList(UserManager.Users, "Id", "UserName");
+
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> RemoveRole([Bind("UserId,RoleId")] RoleUserView ruView)
+        {
+            if (ModelState.IsValid)
+            {
+                var _user = await UserManager.FindByIdAsync(ruView.UserId);
+                var _role = await roleManager.FindByIdAsync(ruView.RoleId);
+                if (_user != null && _role != null)
+                {
+                    var a = await UserManager.RemoveFromRoleAsync(_user, _role.Name);
+                    if (a.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ViewData["RoleId"] = new SelectList(roleManager.Roles, "Id", "Name", ruView.RoleId);
+                        ViewData["UserId"] = new SelectList(UserManager.Users, "Id", "UserName", ruView.UserId);
+                        ViewBag.ErrorMessage = "Failed to assign, try again";
+                        return View(ruView);
+                    }
+
+                }
+                else
+                {
+                    ViewData["RoleId"] = new SelectList(roleManager.Roles, "Id", "Name", ruView.RoleId);
+                    ViewData["UserId"] = new SelectList(UserManager.Users, "Id", "UserName", ruView.UserId);
+                    ViewBag.ErrorMessage = "role or  user not found, try again";
+                    return View(ruView);
+                }
+            }
+            ViewData["RoleId"] = new SelectList(roleManager.Roles, "Id", "Name", ruView.RoleId);
+            ViewData["UserId"] = new SelectList(UserManager.Users, "Id", "UserName", ruView.UserId);
+            return View(ruView);
+
+        }
+
+        private bool AppInfoExists(int id)
+        {
+            return _context.Apps.Any(e => e.AppInfoId == id);
+        }
+    }
+}
---
+++ eStore/Areas/Stores/Controllers/StoreOperationsController.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eStore.BL.Commons;
+using eStore.DL.Data;
+using eStore.Ops;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eStore.Areas.Stores.Controllers
+{
+    public class StoreOperationsController : Controller
+    {
+        private readonly eStoreDbContext _context;
+
+        public StoreOperationsController(eStoreDbContext context)
+        {
+            _context = context;
+        }
+        // GET: StoreOperations
+        public async Task<IActionResult> Index()
+        {
+
+            StoreInfo storeInfo = PostLogin.ReadStoreInfo(HttpContext.Session);
+            int CurrentStore = storeInfo.StoreId;
+            ViewData["UserName"] = storeInfo.UserName;
+            var store = await _context.Stores.FindAsync(CurrentStore);
+            ViewBag.StoreName = store.StoreName;
+            ViewBag.StoreCity = store.City;
+            ViewBag.StoreId = CurrentStore;
+
+            // Check for Today Close and Openning Entry
+            DateTime date = DateTime.Today.Date;
+            //TODO: Update to IST Time zone
+            var openToday = await _context.StoreOpens.Where(c => c.OpenningTime.Date == date.Date && c.StoreId == CurrentStore).FirstOrDefaultAsync();
+            var closeToday = await _context.StoreCloses.Where(c => c.ClosingDate.Date == date.Date && c.StoreId == CurrentStore).FirstOrDefaultAsync();
+            ViewBag.Open = "true";
+            ViewBag.Close = "true";
+            if (openToday != null) ViewBag.Open = "false";
+            if (closeToday != null) ViewBag.Close = "false";
+
+            return View();
+        }
+
+        public async Task<JsonResult> StoreOpenningAsync(int StoreId) {
+
+           int result= await StoreManager.AddStoreOpenningAsync(_context, StoreId);
+            if(result>0)
+            return new JsonResult("ok");
+            else
+                return new JsonResult("error");
+        }
+        public async Task<JsonResult> StoreClosingAsync(int StoreId)
+        {
+            int result = await StoreManager.AddStoreCloseAsync(_context, StoreId);
+            if (result > 0)
+                return new JsonResult("ok");
+            else
+                return new JsonResult("error");
+        }
+        
+    }
+}
