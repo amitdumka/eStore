fa60b8f9eafcffa11530ca4eb58a9599dec39cbb
Amit Kumar <amitnarayansah@gmail.com>
2021-02-10 11:54:51 +0530

    Some Major
    Some thing Error while Commited from VS

---
+++ eStore/ImportDatabase/AprajitaRetailsToeStore.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using eStore.DL.Data;
+using eStore.ImportDatabase.Data;
+using eStore.Shared.Models.Payroll;
+using Microsoft.EntityFrameworkCore;
+
+namespace eStore.ImportDatabase
+{
+    public class AprajitaRetailsToeStore : BasicFunctions
+    {
+        public void ImportPayRollData()
+        {
+
+
+        }
+    }
+
+    public class BasicFunctions
+    {
+        protected eStoreDbContext outDB; AprajitaRetailsDbContext inDB;
+        protected async System.Threading.Tasks.Task GetEmployeeDataAsync()
+        {
+            var inEmpList = await inDB.Employees.ToListAsync();
+
+            List<eStore.Shared.Models.Payroll.Employee> OutEmpList = new List<Shared.Models.Payroll.Employee>();
+            foreach (var inEmp in inEmpList)
+            {
+           
+                List<Attendance> outAttList = new List<Attendance>();
+                var inAttList = await inDB.Attendances.Where(c => c.EmployeeId == inEmp.EmployeeId).ToListAsync();
+                foreach (var inAtt in inAttList)
+                {
+                    Attendance outAtt = new Attendance
+                    {
+                        StoreId = (int?) inAtt.StoreId ?? 1,
+                        AttDate = inAtt.AttDate,
+                        EntryStatus = 0,
+                        IsReadOnly = false,
+                        EntryTime = inAtt.EntryTime,
+                        IsTailoring = (bool?)inAtt.IsTailoring??false,
+                        Remarks = inAtt.Remarks,
+                        Status = inAtt.Status,
+                        UserId = "Admin"
+                    };
+                    outAttList.Add(outAtt);
+                }
+                Employee outEmp = new Employee
+                {
+                    StoreId = (int?)inEmp.StoreId??1,
+                    Attendances=outAttList, UserId="Admin", IsReadOnly =false, State=inEmp.State,
+                    Address=inEmp.Address, AdharNumber=inEmp.AdharNumber, Category=inEmp.Category , City=inEmp.City,
+                    DateOfBirth=(DateTime)inEmp.DateOfBirth, EMail=inEmp.EMail, EntryStatus=0, FatherName=inEmp.FatherName,
+                    FirstName=inEmp.StaffName, HighestQualification=inEmp.HighestQualification, PanNo=inEmp.PanNo,
+                    OtherIdDetails=inEmp.OtherIdDetails, IsTailors=inEmp.IsTailors, MobileNo=inEmp.MobileNo, LastName="",
+                    LeavingDate=inEmp.LeavingDate, IsWorking=inEmp.IsWorking, JoiningDate=inEmp.JoiningDate
+
+                };
+
+            }
+
+        }
+    }
+}
---
+++ eStore/ViewComponents/ChartJsViewComponent.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using eStore.DL.Data;
+using eStore.Shared.ViewModels;
+
+namespace eStore.ViewComponents
+{
+    [ViewComponent(Name = "chartjs")]
+    public class ChartJsViewComponent : ViewComponent
+    {
+        private readonly eStoreDbContext db;
+
+        public ChartJsViewComponent(eStoreDbContext con)
+        {
+            db = con;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var chartData = @"
+            {
+                type: 'line',
+                responsive: true,
+                data:
+                {
+                    labels: ['Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday',  'Sunday'],
+                    datasets: [{
+                        label: 'Weekly Sale',
+                        data: [12000, 19000, 3000, 5000, 2000, 3000,6000],
+                        backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                            ],
+                        borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 159, 64, 1)'
+                            ],
+                        borderWidth: 1
+                    }]
+                },
+                options:
+                {
+                    scales:
+                    {
+                        yAxes: [{
+                            ticks:
+                            {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            }";
+
+            var chart = JsonConvert.DeserializeObject<ChartJs>(chartData);
+
+            var chartModel = new ChartJsViewModel
+            {
+                Chart = chart,
+                ChartJson = JsonConvert.SerializeObject(chart, new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore
+                })
+            };
+
+            return View(chartModel);
+        }
+    }
+
+    public class MonthlySaleData
+    {
+        public List<string> MonthName { get; set; }
+        public List<int> Amount { get; set; }
+    }
+    public class StaffSale
+    {
+        public List<string> StaffName { get; set; }
+
+        public List<int> YearWise { get; set; }
+        public List<int> MonthWise { get; set; }
+        public List<int> CurrentWise { get; set; }
+
+        public StaffSale()
+        {
+            StaffName = new List<string>();
+            YearWise = new List<int>();
+            MonthWise = new List<int>();
+            CurrentWise = new List<int>();
+        }
+    }
+    public class SaleData
+    {
+        public decimal Amount { get; set; }
+        public DateTime OnDate { get; set; }
+    }
+
+    public class SaleDataVM
+    {
+        public List<int> Amount { get; set; }
+        public List<string> Label { get; set; }
+    }
+
+    public static class SaleDataVMExt
+    {
+        public static SaleDataVM CopyObject(this SaleDataVM obj, List<SaleData> data)
+        {
+            List<int> Amt = new List<int>();
+            List<string> label = new List<string>();
+            foreach (var item in data)
+            {
+                Amt.Add((int)item.Amount);
+                label.Add(item.OnDate.ToString("dd/MMMM"));
+            }
+            obj.Amount = Amt; obj.Label = label;
+            return new SaleDataVM { Amount = Amt, Label = label };
+        }
+    }
+    public class YearlySaleData
+    {
+        public List<int> Year { get; set; }
+        public List<int> Amount { get; set; }
+    }
+}
