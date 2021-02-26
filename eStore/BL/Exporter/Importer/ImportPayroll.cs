86d83dccd3b1c4c4d98c1bc0e38c77e0dfb0f576
Amit Kumar <amitnarayansah@gmail.com>
2021-02-26 12:41:15 +0530

    error fixed

--- eStore/BL/Exporter/Importer/ImportPayroll.cs
+++ eStore/BL/Exporter/Importer/ImportPayroll.cs
@@ -13,14 +13,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace eStore.BL.Exporter.Database
 {
-    class EmpCodes
-    {
-        public int OldId { get; set; }
-        public int NewId { get; set; }
-        public string StaffName { get; set; }
-        public int PartyId { get; set; }
+    //class EmpCodes
+    //{
+    //    public int OldId { get; set; }
+    //    public int NewId { get; set; }
+    //    public string StaffName { get; set; }
+    //    public int PartyId { get; set; }
 
-    }
+    //}
 
     class ImportPayroll
     {
