fb62566a301b0e6b9a08d9c6bbe7d361e32202e8
Amit Kumar <amitnarayansah@gmail.com>
2021-06-09 09:26:27 +0530

    voy Ui onbase

---
+++ eStore/Areas/Accounts/Controllers/SaleWithCustomersController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using eStore.DL.Data;
+using eStore.Shared.Uploader;
+
+namespace eStore.Areas.Accounts.Controllers
+{
+    [Area("Accounts")]
+    public class SaleWithCustomersController : Controller
+    {
+        private readonly eStoreDbContext _context;
+
+        public SaleWithCustomersController(eStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Accounts/SaleWithCustomers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.SaleWithCustomers.ToListAsync());
+        }
+
+        // GET: Accounts/SaleWithCustomers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var saleWithCustomer = await _context.SaleWithCustomers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (saleWithCustomer == null)
+            {
+                return NotFound();
+            }
+
+            return View(saleWithCustomer);
+        }
+
+        // GET: Accounts/SaleWithCustomers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Accounts/SaleWithCustomers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,InvoiceNo,InvoiceDate,InvoiceType,Quantity,BillAmt,PaymentType,CustomerName,Address,Phone,Email,DateofBirth")] SaleWithCustomer saleWithCustomer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(saleWithCustomer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(saleWithCustomer);
+        }
+
+        // GET: Accounts/SaleWithCustomers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var saleWithCustomer = await _context.SaleWithCustomers.FindAsync(id);
+            if (saleWithCustomer == null)
+            {
+                return NotFound();
+            }
+            return View(saleWithCustomer);
+        }
+
+        // POST: Accounts/SaleWithCustomers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,InvoiceNo,InvoiceDate,InvoiceType,Quantity,BillAmt,PaymentType,CustomerName,Address,Phone,Email,DateofBirth")] SaleWithCustomer saleWithCustomer)
+        {
+            if (id != saleWithCustomer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(saleWithCustomer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SaleWithCustomerExists(saleWithCustomer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(saleWithCustomer);
+        }
+
+        // GET: Accounts/SaleWithCustomers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var saleWithCustomer = await _context.SaleWithCustomers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (saleWithCustomer == null)
+            {
+                return NotFound();
+            }
+
+            return View(saleWithCustomer);
+        }
+
+        // POST: Accounts/SaleWithCustomers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var saleWithCustomer = await _context.SaleWithCustomers.FindAsync(id);
+            _context.SaleWithCustomers.Remove(saleWithCustomer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SaleWithCustomerExists(int id)
+        {
+            return _context.SaleWithCustomers.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ eStore/Areas/Accounts/Controllers/TaxRegistersController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using eStore.DL.Data;
+using eStore.Shared.Uploader;
+
+namespace eStore.Areas.Accounts.Controllers
+{
+    [Area("Accounts")]
+    public class TaxRegistersController : Controller
+    {
+        private readonly eStoreDbContext _context;
+
+        public TaxRegistersController(eStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Accounts/TaxRegisters
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TaxRegisters.ToListAsync());
+        }
+
+        // GET: Accounts/TaxRegisters/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var taxRegister = await _context.TaxRegisters
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (taxRegister == null)
+            {
+                return NotFound();
+            }
+
+            return View(taxRegister);
+        }
+
+        // GET: Accounts/TaxRegisters/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Accounts/TaxRegisters/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,InvoiceNo,InvoiceDate,InvoiceType,BrandName,ProductName,ItemDesc,BARCODE,StyleCode,Quantity,SaleValue,DiscountAmt,BasicAmt,TaxAmt,TaxRate,TaxDesc,BillAmt")] TaxRegister taxRegister)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(taxRegister);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(taxRegister);
+        }
+
+        // GET: Accounts/TaxRegisters/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var taxRegister = await _context.TaxRegisters.FindAsync(id);
+            if (taxRegister == null)
+            {
+                return NotFound();
+            }
+            return View(taxRegister);
+        }
+
+        // POST: Accounts/TaxRegisters/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,InvoiceNo,InvoiceDate,InvoiceType,BrandName,ProductName,ItemDesc,BARCODE,StyleCode,Quantity,SaleValue,DiscountAmt,BasicAmt,TaxAmt,TaxRate,TaxDesc,BillAmt")] TaxRegister taxRegister)
+        {
+            if (id != taxRegister.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(taxRegister);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TaxRegisterExists(taxRegister.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(taxRegister);
+        }
+
+        // GET: Accounts/TaxRegisters/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var taxRegister = await _context.TaxRegisters
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (taxRegister == null)
+            {
+                return NotFound();
+            }
+
+            return View(taxRegister);
+        }
+
+        // POST: Accounts/TaxRegisters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var taxRegister = await _context.TaxRegisters.FindAsync(id);
+            _context.TaxRegisters.Remove(taxRegister);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TaxRegisterExists(int id)
+        {
+            return _context.TaxRegisters.Any(e => e.Id == id);
+        }
+    }
+}
