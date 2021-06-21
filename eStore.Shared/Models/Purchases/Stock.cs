6cbd96a0b49cd89af147c9e40f779fd9be7d6873
Amit Kumar <amitnarayansah@gmail.com>
2021-06-21 23:00:51 +0530

    stock

--- eStore.Shared/Models/Purchases/Stock.cs
+++ eStore.Shared/Models/Purchases/Stock.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eStore.Shared.Models.Purchases
 {
@@ -9,23 +10,17 @@ namespace eStore.Shared.Models.Purchases
     public class Stock : BaseSNT
     {
         public int StockId { set; get; }
-
         [Display(Name = "Product")]
-
         public int? ProductItemId { set; get; }
-        public string BarCode { get; set; }
+        public string Barcode { get; set; }
+        [ForeignKey("Barcode")]
         public virtual ProductItem ProductItem { get; set; }
-        
-        
-
         public double Quantity { get { return PurchaseQty - SaleQty - HoldQty; } }
-
         [Display(Name = "Sale Qty")]
         public double SaleQty { get; set; }
-
         [Display(Name = "Purchase Qty")]
         public double PurchaseQty { get; set; }
-
+        [Display(Name = "Hold Qty")]
         public double HoldQty { get; set; }
 
         public Unit Units { get; set; }
