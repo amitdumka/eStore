3fd92d403798e7ad1ed8a400405e7e130b4ca709
Amit Kumar <amitnarayansah@gmail.com>
2021-07-05 21:52:48 +0530

    ok

---
+++ dbo.Salesmen.data.sql
@@ -0,0 +1,12 @@
+﻿SET IDENTITY_INSERT [dbo].[Salesmen] ON
+INSERT INTO [dbo].[Salesmen] ([SalesmanId], [SalesmanName], [EmployeeId], [UserId], [EntryStatus], [IsReadOnly], [StoreId]) VALUES (1, N'MANAGER', NULL, N'AA', 0, 1, 1)
+INSERT INTO [dbo].[Salesmen] ([SalesmanId], [SalesmanName], [EmployeeId], [UserId], [EntryStatus], [IsReadOnly], [StoreId]) VALUES (5, N'Mukesh Kumar', NULL, N'AA', 0, 0, 1)
+INSERT INTO [dbo].[Salesmen] ([SalesmanId], [SalesmanName], [EmployeeId], [UserId], [EntryStatus], [IsReadOnly], [StoreId]) VALUES (6, N'Sanjeev Kumar Mishra', NULL, N'AA', 0, 1, 1)
+INSERT INTO [dbo].[Salesmen] ([SalesmanId], [SalesmanName], [EmployeeId], [UserId], [EntryStatus], [IsReadOnly], [StoreId]) VALUES (7, N'Sourav Turi', NULL, N'AA', 0, 1, 1)
+INSERT INTO [dbo].[Salesmen] ([SalesmanId], [SalesmanName], [EmployeeId], [UserId], [EntryStatus], [IsReadOnly], [StoreId]) VALUES (8, N'Alok', NULL, N'AA', 0, 1, 1)
+INSERT INTO [dbo].[Salesmen] ([SalesmanId], [SalesmanName], [EmployeeId], [UserId], [EntryStatus], [IsReadOnly], [StoreId]) VALUES (9, N'Amit Thakur', NULL, N'AA', 0, 1, 1)
+INSERT INTO [dbo].[Salesmen] ([SalesmanId], [SalesmanName], [EmployeeId], [UserId], [EntryStatus], [IsReadOnly], [StoreId]) VALUES (10, N'AJAM MASTER', NULL, N'AA', 0, 1, 1)
+INSERT INTO [dbo].[Salesmen] ([SalesmanId], [SalesmanName], [EmployeeId], [UserId], [EntryStatus], [IsReadOnly], [StoreId]) VALUES (11, N'Santosh Kumar', NULL, N'AA', 0, 1, 1)
+INSERT INTO [dbo].[Salesmen] ([SalesmanId], [SalesmanName], [EmployeeId], [UserId], [EntryStatus], [IsReadOnly], [StoreId]) VALUES (12, N'Bikas Kumar Sah', NULL, N'AA', 0, 1, 1)
+INSERT INTO [dbo].[Salesmen] ([SalesmanId], [SalesmanName], [EmployeeId], [UserId], [EntryStatus], [IsReadOnly], [StoreId]) VALUES (13, N'Jafer master', NULL, N'AA', 0, 1, 1)
+SET IDENTITY_INSERT [dbo].[Salesmen] OFF
